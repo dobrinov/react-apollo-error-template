910968b9059cd51f1dfcf1c11224a6a47659c593
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2020-09-23 13:25:29 +0300

    Setup reproduction environment

--- src/App.js
+++ src/App.js
@@ -1,81 +1,89 @@
-import React, { useState } from "react";
-import { gql, useQuery, useMutation } from "@apollo/client";
+import React, { useCallback } from "react";
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  useQuery,
+  NetworkStatus,
+  gql
+} from "@apollo/client";
+import { relayStylePagination } from "@apollo/client/utilities";
 
-const ALL_PEOPLE = gql`
-  query AllPeople {
-    people {
-      id
-      name
+const client = new ApolloClient({
+  uri: "https://gitlab.com/api/graphql",
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          projects: relayStylePagination()
+        }
+      }
     }
-  }
-`;
+  })
+});
 
-const ADD_PERSON = gql`
-  mutation AddPerson($name: String) {
-    addPerson(name: $name) {
-      id
-      name
+const PROJECTS_QUERY = gql`
+  query ProjectsQuery($after: String) {
+    projects(first: 1, after: $after) {
+      edges {
+        node {
+          id
+          name
+        }
+      }
+      pageInfo {
+        endCursor
+        hasNextPage
+      }
     }
   }
 `;
 
 export default function App() {
-  const [name, setName] = useState('');
-  const {
-    loading,
-    data,
-  } = useQuery(ALL_PEOPLE);
+  return (
+    <ApolloProvider client={client}>
+      <Reproduction />
+    </ApolloProvider>
+  );
+}
 
-  const [addPerson] = useMutation(ADD_PERSON, {
-    update: (cache, { data: { addPerson: addPersonData } }) => {
-      const peopleResult = cache.readQuery({ query: ALL_PEOPLE });
+function Reproduction() {
+  const { data, loading, error, networkStatus, fetchMore } = useQuery(
+    PROJECTS_QUERY,
+    {
+      notifyOnNetworkStatusChange: true
+    }
+  );
 
-      cache.writeQuery({
-        query: ALL_PEOPLE,
-        data: {
-          ...peopleResult,
-          people: [
-            ...peopleResult.people,
-            addPersonData,
-          ],
-        },
+  const onLoadMore = useCallback(
+    (after) => {
+      fetchMore({
+        query: PROJECTS_QUERY, // Comment this line and you will get the expected behaviour
+        variables: { after }
       });
     },
-  });
+    [fetchMore]
+  );
+
+  const fetchingMore = networkStatus === NetworkStatus.fetchMore;
+
+  if (fetchingMore) return <div>Fetching more ...</div>;
+  if (loading) return <div>Loading ...</div>;
+  if (error) return <div>{JSON.stringify(error)}</div>;
+
+  const projects = data.projects.edges.map((edge) => edge.node);
+  const after = data.projects.pageInfo.endCursor;
 
   return (
-    <main>
-      <h1>Apollo Client Issue Reproduction</h1>
-      <p>
-        This application can be used to demonstrate an error in Apollo Client.
-      </p>
-      <div className="add-person">
-        <label htmlFor="name">Name</label>
-        <input 
-          type="text" 
-          name="name" 
-          value={name}
-          onChange={evt => setName(evt.target.value)}
-        />
-        <button
-          onClick={() => {
-            addPerson({ variables: { name } });
-            setName('');
-          }}
-        >
-          Add person
-        </button>
-      </div>
-      <h2>Names</h2>
-      {loading ? (
-        <p>Loading…</p>
-      ) : (
-        <ul>
-          {data?.people.map(person => (
-            <li key={person.id}>{person.name}</li>
-          ))}
-        </ul>
-      )}
-    </main>
+    <div>
+      <button type="button" onClick={() => onLoadMore(after)}>
+        Fetch more
+      </button>
+      <ul>
+        {projects.map((node) => (
+          <li key={node.id}>{node.name}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
